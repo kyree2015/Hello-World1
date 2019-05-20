e7c46ea4dd6f945aad2541914a13d3e423275cf0
LeAnne McQuilkin <bajanbeauty21@gmail.com>
2019-05-19 19:17:50 -0700

    Add files via upload

---
+++ Hello_World1.py
@@ -0,0 +1,13 @@
+print("Hello World")
+name = "Noelle"
+print("My name is", name)
+name = "Noelle"
+print("My name is " + name)
+name = 42
+print("I am" , name)
+fave_food1 = "sushi"
+fave_food2 = "pizza"
+print(f"My favorite food is {fave_food1} and {fave_food2}")
+fave_food1 = "sushi"
+fave_food2 = "pizza"
+print("My favorite food is {} and {}" .format(fave_food2, fave_food1))
